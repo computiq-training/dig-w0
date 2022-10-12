c958c47aa777f89954e66b3784acb3c32f3303fe
Ali Abbas <35330585+Ali-Abbas-Jumaa@users.noreply.github.com>
2022-10-12 20:03:09 +0300

    Solution

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
-(async ()=>{
+
+(async () => {
     console.log('App Started');
     const PERSONS = [
         {
-            name:'Ahmed Ali',
-            age:20
+            name: 'Ahmed Ali',
+            age: 20
         },
         {
-            name:'Saif Amer',
-            age:19
+            name: 'Saif Amer',
+            age: 19
         },
         {
-            name:'Marwan Ali',
-            age:30
+            name: 'Marwan Ali',
+            age: 30
         },
         {
-            name:'Ameer Qais',
-            age:16
+            name: 'Ameer Qais',
+            age: 16
         },
         {
-            name:'Nabeel Fawzi',
-            age:45
+            name: 'Nabeel Fawzi',
+            age: 45
         },
         undefined
         ,
         {
-            name:'Ali Hasan',
-            age:45
+            name: 'Ali Hasan',
+            age: 45
         }
     ];
-    // TO-DO
-    // Promise object/function here
-    // 2 params, 1: list, 2: target to match
-    // return: Promise, resolve: list after filteration, reject: error message
-    // TO-DO
+
+    filteringPersonsAges(PERSONS, 30)
+        .then((res) => console.log(res))
+        .catch((e) => console.log(e))
+
 })();
+
+function filteringPersonsAges(list, age) {
+    let p = new Promise((resolve, reject) => {
+
+        var results = list.filter(function (person) {
+            // console.log(person)
+            if (person) {
+                return person.age >= age
+            }
+        }
+        );
+        if (results.length != 0) {
+            resolve(results);
+        }
+        else {
+            reject('there is no persone with same age!!!');
+        }
+    })
+    return p
+}
