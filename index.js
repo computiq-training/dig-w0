9530290446ecfa53175159f71ea254d690b4c1bd
ENG-MTH <99608517+ENG-MTH@users.noreply.github.com>
2022-10-14 09:49:09 +0300

    first up

--- index.js
+++ index.js
@@ -21,7 +21,7 @@
             name:'Nabeel Fawzi',
             age:45
         },
-        undefined
+        
         ,
         {
             name:'Ali Hasan',
@@ -33,4 +33,26 @@
     // 2 params, 1: list, 2: target to match
     // return: Promise, resolve: list after filteration, reject: error message
     // TO-DO
+
+
+    const fun =(list1, Target=45)=>{
+        return promis = new Promise((resolve,reject)=>{
+        const Fillterd_list= PERSONS.filter((person)=> Target== person.age)
+           
+
+        let conditon= true
+            if (conditon) {
+                resolve(Fillterd_list);
+              } else {
+                reject("err");
+              }
+            
+
+    })
+    
+
+    }
+    
+    
+    fun().then(result=>console.log(result)).catch(err=> console.log(err))
 })();
