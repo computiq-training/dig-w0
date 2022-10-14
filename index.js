4ea35b9ef1e2599525a2777829e21b72f0756370
dhay0.00 <90208082+dhay01@users.noreply.github.com>
2022-10-15 00:29:40 +0300

    solution

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-(async ()=>{
+(async () => {
     console.log('App Started');
     const PERSONS = [
         {
-            name:'Ahmed Ali',
-            age:20
+            name: 'Ahmed Ali',
+            age: 20
         },
         {
-            name:'Saif Amer',
-            age:19
+            name: 'Saif Amer',
+            age: 19
         },
         {
-            name:'Marwan Ali',
-            age:30
+            name: 'Marwan Ali',
+            age: 30
         },
         {
-            name:'Ameer Qais',
-            age:16
+            name: 'Ameer Qais',
+            age: 16
         },
         {
-            name:'Nabeel Fawzi',
-            age:45
+            name: 'Nabeel Fawzi',
+            age: 45
         },
         undefined
         ,
         {
-            name:'Ali Hasan',
-            age:45
+            name: 'Ali Hasan',
+            age: 45
         }
     ];
     // TO-DO
@@ -33,4 +33,26 @@
     // 2 params, 1: list, 2: target to match
     // return: Promise, resolve: list after filteration, reject: error message
     // TO-DO
+
+
+    let promise = new Promise((res, error) => {
+        let target_age = 45;
+
+        let person = PERSONS.filter((item) => {
+
+            if ((item) && (item.age === target_age))
+                return item;
+        })
+        if (person.length > 0) res(person);
+        else error('Reject');
+    })
+
+    promise.then((res) => {
+        console.log(res);
+    })
+        .catch((err) => {
+            console.error(err);
+        })
+
+
 })();
